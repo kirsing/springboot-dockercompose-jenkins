b8067ee1bdc1082801aa60f22cbf64590edec68d
kirsing <kirsing98@icloud.com>
2022-09-24 22:00:16 +0300

    fixed pipeline again1

--- src/test/java/com/example/springbootjenkins/SpringbootJenkinsApplicationTests.java
+++ src/test/java/com/example/springbootjenkins/SpringbootJenkinsApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.springbootjenkins;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active:prod")
 class SpringbootJenkinsApplicationTests {
 
     @Test
